c962f4c7952a53a6d962d048b71bfbf271ae2bd8
Liangyu Wu <133572075+Liangyu5wu@users.noreply.github.com>
2025-03-11 14:58:08 -0700

    Create load_bitstream.tcl

---
+++ load_bitstream.tcl
@@ -0,0 +1,20 @@
+open_hw
+connect_hw_server
+open_hw_target
+
+set BITFILE_PATH "GBCR2_SEU_Test.bit"
+
+set current_device [lindex [get_hw_devices] 0]
+refresh_hw_device -update_hw_probes false $current_device
+set_property PROGRAM.FILE $BITFILE_PATH $current_device
+
+# Program the device and check the result
+if {[catch {program_hw_devices $current_device} result]} {
+    puts "Programming failed! Error: $result"
+} else {
+    puts "Programming successful!"
+}
+
+close_hw_target
+disconnect_hw_server
+close_hw
