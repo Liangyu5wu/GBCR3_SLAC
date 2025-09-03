f76a0a11af74b890f007eed523643630fbe6ab6f
Liangyu Wu <133572075+Liangyu5wu@users.noreply.github.com>
2025-09-03 15:10:13 -0700

    Update load_bitstream.tcl

--- load_bitstream.tcl
+++ load_bitstream.tcl
@@ -3,6 +3,7 @@ connect_hw_server
 open_hw_target
 
 set BITFILE_PATH "GBCR2_SEU_Test.bit"
+#set BITFILE_PATH "firmware/GBCR2_SEU_Test.bit"
 
 set current_device [lindex [get_hw_devices] 0]
 refresh_hw_device -update_hw_probes false $current_device
